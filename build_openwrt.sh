7e00171360d972c66b6ce096bd4e27a6bed5ead6
brinkqiang <brink.qiang@gmail.com>
2024-07-14 21:58:28 +0800

    add

--- build_openwrt.sh
+++ build_openwrt.sh
@@ -25,12 +25,12 @@ pushd thirdparty/curl
 # autoreconf -i
 popd
 
-find . -name "*.cmake.in" -exec sed -i  "s/-lrt//g" '{}' \;
-find . -name "*.cmake.in" -exec sed -i  "s/-ldl//g" '{}' \;
-find . -name "link.txt" -exec sed -i  "s/-ldl//g" '{}' \;
-find . -name "link.txt" -exec sed -i  "s/-lrt//g" '{}' \;
-find . -name Makefile2 -exec sed -i  "s/-ldl//g" '{}' \;
-find . -name Makefile2 -exec sed -i  "s/-lrt//g" '{}' \;
+#find . -name "*.cmake.in" -exec sed -i  "s/-lrt//g" '{}' \;
+#find . -name "*.cmake.in" -exec sed -i  "s/-ldl//g" '{}' \;
+#find . -name "link.txt" -exec sed -i  "s/-ldl//g" '{}' \;
+#find . -name "link.txt" -exec sed -i  "s/-lrt//g" '{}' \;
+#find . -name Makefile2 -exec sed -i  "s/-ldl//g" '{}' \;
+#find . -name Makefile2 -exec sed -i  "s/-lrt//g" '{}' \;
 
 pushd build
 
