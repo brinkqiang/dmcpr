039d8c0569ed718d602380cd0cb3bf1ca83304aa
brinkqiang <brink.qiang@gmail.com>
2024-07-14 21:44:07 +0800

    add

---
+++ build_openwrt.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# - install depends tools
+# yum -y install git
+# yum -y install gcc gcc-c++ autoconf libtool automake make
+#
+
+# - clone code
+# git clone https://github.com/brinkqiang/dmcurl.git
+# pushd dmcurl
+# git submodule update --init --recursive
+#
+
+# pushd depends_path
+# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
+# sh configure
+# popd
+
+rm -rf build
+mkdir build
+pushd build
+
+pushd thirdparty/curl
+./buildconf
+./configure --disable-dependency-tracking
+# autoreconf -i
+popd
+
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+make -j1
+popd
+# popd
