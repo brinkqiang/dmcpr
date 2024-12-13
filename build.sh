b892e2335c03cb1d3a5c8e69236ca1bada8f0246
brinkqiang <brrink.qiang@gmail.com>
2024-12-13 15:44:35 +0800

    --wip-- [skip ci]

--- build.sh
+++ build.sh
@@ -5,26 +5,16 @@
 # yum -y install gcc gcc-c++ autoconf libtool automake make
 #
 
-# - clone code
-# git clone https://github.com/brinkqiang/dmcurl.git
-# pushd dmcurl
-# git submodule update --init --recursive
-#
-
-# pushd thirdparty/depends_path
-# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
-# sh configure
-# popd
-
 rm -rf build
 mkdir -p build
-pushd build
+
+# pushd build
+cd build
 
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
 
-popd
-
 # popd
+cd ..
 
 # echo continue && read -n 1
